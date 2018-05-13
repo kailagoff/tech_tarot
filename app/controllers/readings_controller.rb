edca578eee9e943ce2da609d3c6399eddd3d2322
kailagoff <kaila.goff@gmail.com>
2018-05-12 20:09:17 -0700

    add javascript to reading page

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -3,6 +3,9 @@ class ReadingsController < ApplicationController
   def index
     @cards = Card.threeCards()
     @focus = Card.focusCard()
+    respond_to do |format|
+        format.html 
+        format.js
+    end
   end
-
 end
