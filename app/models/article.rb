5aecf9ba2074f71c2e393b39bbca63d0a8446f84
kailagoff <kaila.goff@gmail.com>
2018-05-14 15:46:47 -0700

    change api call to search for tarot articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,13 @@
 class Article
   def make_NYT_API_call
-    response = RestClient::Request.execute(method: :get, url: 'http://api.nytimes.com/svc/books/v3/lists/names.json', headers: {api_key: ENV['NYT_API_KEY']})
-    JSON.parse(response)
+    uri = URI("https://api.nytimes.com/svc/search/v2/articlesearch.json")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    uri.query = URI.encode_www_form({
+      "api-key" => "2025835c97874ed7b3bf98c1f9bb0a94",
+      "q" => "tarot"
+    })
+    request = Net::HTTP::Get.new(uri.request_uri)
+    @result = JSON.parse(http.request(request).body)
   end
 end
