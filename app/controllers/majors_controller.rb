6a89c87120c1283d9628dd1e5bf23f2195d2896c
kailagoff <kaila.goff@gmail.com>
2018-05-14 14:08:22 -0700

    combine minor controller and views with major

--- app/controllers/majors_controller.rb
+++ app/controllers/majors_controller.rb
@@ -3,6 +3,10 @@ class MajorsController < ApplicationController
   def index
     @cards = Card.all.order(:id)
     @arcana_major = @cards.major_arcana
+    @amazon_minor = @cards.minor_amazon
+    @microsoft_minor = @cards.minor_microsoft
+    @google_minor = @cards.minor_google
+    @oracle_minor = @cards.minor_oracle
   end
 
 end
