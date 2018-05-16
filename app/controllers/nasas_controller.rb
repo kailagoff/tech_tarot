633777012ac911dfe0ae5e40a72b7c7e93374dee
kailagoff <kaila.goff@gmail.com>
2018-05-16 10:04:24 -0700

    add AJAX to card content on NASA page, both descriptions responding to same button

--- app/controllers/nasas_controller.rb
+++ app/controllers/nasas_controller.rb
@@ -7,6 +7,7 @@ class NasasController < ApplicationController
   end
 
   def new
+    @card = Card.oneCard()
     @nasa_info = Nasa.get_api_info(params[:date] || Time.now.in_time_zone('US/Eastern').to_date.to_s)
   	@media_type_is_video = @nasa_info['media_type'].eql?("video")
     respond_to do |format|
