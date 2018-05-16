fed871f6d4ac82286bcae1a1f6c37949c770988d
kailagoff <kaila.goff@gmail.com>
2018-05-16 09:46:57 -0700

    add AJAX to content on NASA image of the day

--- app/controllers/nasas_controller.rb
+++ app/controllers/nasas_controller.rb
@@ -6,4 +6,14 @@ class NasasController < ApplicationController
   	@media_type_is_video = @nasa_info['media_type'].eql?("video")
   end
 
+  def new
+    @nasa_info = Nasa.get_api_info(params[:date] || Time.now.in_time_zone('US/Eastern').to_date.to_s)
+  	@media_type_is_video = @nasa_info['media_type'].eql?("video")
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { render 'nasas/new.js.erb'}
+      format.js
+     end
+  end
+
 end
